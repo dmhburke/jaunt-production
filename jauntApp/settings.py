9262b16b60ddbacb0da00073cb8e9381ef3fba0a
Daniel Burke <dmhburke@gmail.com>
2020-08-25 16:04:09 -0400

    update settings to deployment

--- jauntApp/settings.py
+++ jauntApp/settings.py
@@ -128,33 +128,33 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
-STATIC_URL = '/static/'
+# STATIC_URL = '/static/'
 
 # DEPLOYMENT
-# from os import environ as CONFIG
-#
-# AWS_LOCATION = 'static'
-# AWS_ACCESS_KEY_ID = CONFIG['AWS_ACCESS_KEY_ID']
-# AWS_SECRET_ACCESS_KEY = CONFIG['AWS_SECRET_ACCESS_KEY']
-# AWS_STORAGE_BUCKET_NAME = CONFIG['AWS_STORAGE_BUCKET_NAME']
-# AWS_S3_CUSTOM_DOMAIN='%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
-#
-# AWS_S3_OBJECT_PARAMETERS = {
-#      'CacheControl': 'max-age=86400',
-# }
-# DEFAULT_FILE_STORAGE = 'jauntApp.storage_backends.MediaStorage'
-# STATICFILES_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
-# STATICFILES_DIRS = [
-#     os.path.join(BASE_DIR, 'catalog/static'),
-# ]
-# STATIC_URL='https://%s/%s/' % (AWS_S3_CUSTOM_DOMAIN, AWS_LOCATION)
-# ADMIN_MEDIA_PREFIX = STATIC_URL + 'admin/'
-# STATICFILES_FINDERS = ('django.contrib.staticfiles.finders.FileSystemFinder', 'django.contrib.staticfiles.finders.AppDirectoriesFinder',)
-# AWS_DEFAULT_ACL = None
-# AWS_PRELOAD_METADATA=True
+from os import environ as CONFIG
 
-## ENABLE SSL
-# SECURE_SSL_REDIRECT = True
+AWS_LOCATION = 'static'
+AWS_ACCESS_KEY_ID = CONFIG['AWS_ACCESS_KEY_ID']
+AWS_SECRET_ACCESS_KEY = CONFIG['AWS_SECRET_ACCESS_KEY']
+AWS_STORAGE_BUCKET_NAME = CONFIG['AWS_STORAGE_BUCKET_NAME']
+AWS_S3_CUSTOM_DOMAIN='%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
+
+AWS_S3_OBJECT_PARAMETERS = {
+     'CacheControl': 'max-age=86400',
+}
+DEFAULT_FILE_STORAGE = 'jauntApp.storage_backends.MediaStorage'
+STATICFILES_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'catalog/static'),
+]
+STATIC_URL='https://%s/%s/' % (AWS_S3_CUSTOM_DOMAIN, AWS_LOCATION)
+ADMIN_MEDIA_PREFIX = STATIC_URL + 'admin/'
+STATICFILES_FINDERS = ('django.contrib.staticfiles.finders.FileSystemFinder', 'django.contrib.staticfiles.finders.AppDirectoriesFinder',)
+AWS_DEFAULT_ACL = None
+AWS_PRELOAD_METADATA=True
+
+# ENABLE SSL
+SECURE_SSL_REDIRECT = True
 
 ##== MEDIA SETTINGS
 
